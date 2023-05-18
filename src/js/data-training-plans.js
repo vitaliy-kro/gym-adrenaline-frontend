2b71ba35489158cbf6b4af08d586e1af50538be6
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-05-18 17:23:34 +0300

    update services

---
+++ src/js/data-training-plans.js
@@ -0,0 +1,68 @@
+const plans = [
+    {name: 'Разове тренування',
+    duration: '1 день',
+    numbers: '1',
+    cost: '80 UAH'
+    },
+    {name: '8 тренувань',
+    duration: '1 місяць',
+    numbers: '8',
+    cost: '440 UAH'
+    },
+    {name: '12 тренувань',
+    duration: '1 місяць',
+    numbers: '12',
+    cost: '500 UAH'
+    },
+    {name: 'Безліміт',
+    duration: '1 місяць',
+    numbers: 'необмежено',
+    cost: '550 UAH'
+    },
+    {name: 'Квартальний',
+    duration: '3 місяці',
+    numbers: 'необмежено',
+    cost: '460UAH/month'
+    },
+    {name: 'Піврічний',
+    duration: '6 місяців',
+    numbers: 'необмежено',
+    cost: '430UAH/month'
+    },
+    {name: 'Річний',
+    duration: '1 рік',
+    numbers: 'необмежено',
+    cost: '400UAH/month'
+    }
+];
+const persTrener = [
+    {name: '1 тренування з тренером',
+    duration: '1 тренування',
+    numbers: '1',
+    cost: '250 UAH'
+    },
+    {name: '4 тренування з тренером',
+    duration: '1 місяць',
+    numbers: '4',
+    cost: '800 UAH'
+    },
+    {name: '12 тренувань з тренером',
+    duration: '1 місяць',
+    numbers: '12',
+    cost: '2200 UAH'
+    }
+];
+const progPlans = [
+    {name: 'Персональна програма тренувань',
+    duration: '1 місяць',
+    numbers: '1',
+    cost: '200 UAH'
+    },
+    {name: 'Персональна програма харчування',
+    duration: '1 тиждень',
+    numbers: '1',
+    cost: '100 UAH'
+    }
+]
+export {plans, persTrener, progPlans}
+
--- src/js/section-services.js
+++ src/js/section-services.js
@@ -1,15 +1,25 @@
+import {plans, persTrener, progPlans} from './data-training-plans'
 const trainingList = document.querySelector('.js-training');
 const trenerPlan = document.querySelector('.js-trener');
 const programPlan = document.querySelector('.js-program-plan');
 const divMore = document.querySelector('.js-more')
 
-addEventListener.trainingList('click', OnLoadMore);
-addEventListener.trenerPlan('click', OnLoadMore);
-addEventListener.programPlan('click', OnLoadMore);
+addEventListener.trainingList('click', divMore.insertAdjacentHTML('beforeend', OnLoadMore(plans)));
+addEventListener.trenerPlan('click', divMore.insertAdjacentHTML('beforeend', OnLoadMore(persTrener)));
+addEventListener.programPlan('click', divMore.insertAdjacentHTML('beforeend', OnLoadMore(progPlans)));
 
 
 
-function OnLoadMore() {
-divMore.insertAdjacentHTML("beforeend", makupTraining)
+function OnLoadMore(data) {
+    return data.map(({ name, duration, numbers, cost }) => {
+        return  `<ul class="service-item-more">
+                    <li>${name}</li>
+                    <li><span>Тривалість:</span> ${duration}</li> 
+                    <li><span>Кількість:</span> ${numbers}</li>
+                    <li><span>Вартість:</span> ${cost}</li>
+                    
+                </ul>`
+        
+        
+    }).join("")
 }
-const makupTraining = "";
