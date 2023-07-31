b62d846bd62823c47cd5c26e137ce579c1796681
vitaliy-kro <99730269+vitaliy-kro@users.noreply.github.com>
2023-07-31 08:31:35 +0000

    Deploying to gh-pages from @ vitaliy-kro/gym-adrenaline-frontend@e22a16db774f07d542c06dd9b12ff101772ff419 🚀

---
+++ index.55e8fe18.js
@@ -0,0 +1,2 @@
+function readMore(t){t.parentElement.querySelector(".content").classList.toggle("active"),"Читати більше"==t.textContent?t.textContent="Показувати менше":t.textContent="Читати більше"}function showMore(t){t.parentElement.querySelector(".view").classList.toggle("active"),"Порахувати"==t.textContent?t.textContent="Сховати":t.textContent="Порахувати"}
+//# sourceMappingURL=index.55e8fe18.js.map
